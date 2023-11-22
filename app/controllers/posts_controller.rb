f7af22a0db33cbaaca60936835c28b419d937c61
Bubinator <vladonnumber1@gmail.com>
2023-11-22 20:53:25 +0200

    Add authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+
+  http_basic_authenticate_with name: "admin", password: "goko",
+    except: [:index, :show]
+
   def index
     @post = Post.all
   end
